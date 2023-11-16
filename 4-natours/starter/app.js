ebcc4ab475c8a4fe63385a62fea18ba0fc52df4b
unknown <codyt11@hotmail.com>
2023-11-15 20:55:03 -0600

    set up basic server with first get req

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'hello from the server', app: 'Natours' });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
